99f66b322fb98bb5e08a5b864b22ace6e772e53b
Patrick Furtak <patrick@patrickfurtak.com>
2020-04-06 15:51:04 -0400

    used CRA, created activity component, scaffolded out Redux structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Activity from './components/Activity';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Activity />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Activity.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Activity = () => {
+  return (
+    <div>
+      <h1>Activity Component</h1>
+    </div>
+  );
+};
+
+export default Activity;
